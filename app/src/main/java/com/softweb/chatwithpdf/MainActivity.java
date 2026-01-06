a86e1d807dbc191d03c0ee8224a4cf9930a293d4
Om Waman <omwaman1@gmail.com>
2026-01-06 18:26:29 +0530

    Update app logo and Play Store developer URL

--- app/src/main/java/com/softweb/chatwithpdf/MainActivity.java
+++ app/src/main/java/com/softweb/chatwithpdf/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
     
     // URLs
     private static final String PRIVACY_POLICY_URL = "https://omwaman1.github.io/chatwithpdf/privacy-policy.html";
-    private static final String PLAY_STORE_URL = "https://play.google.com/store/apps/developer?id=Softweb+Technologies";
+    private static final String PLAY_STORE_URL = "https://play.google.com/store/apps/dev?id=Softweb_Technologies";
     
     private Uri pdfUri;
     private String pdfText = "";
